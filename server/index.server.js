ed33aaa15605cd961cb4f24ed23a765cec750369
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-02-27 14:04:39 +0530

    Server initialization

---
+++ server/index.server.js
@@ -0,0 +1,12 @@
+const express =require('express')
+const app=express();
+const PORT=9000;
+
+app.get(`/song`,(req,res)=>{
+    res.send("Hello from server");
+});
+
+app.listen(PORT,()=>
+{
+    console.log(`Server listening on port ${PORT}`);  
+})
