d7cea15dee051a4c474cd98233224d3e8133eb41
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-02-29 23:59:23 +0530

    mock

--- server/index.server.js
+++ server/index.server.js
@@ -1,9 +1,9 @@
-const express =require('express')
-const app=express();
-const fs = require('fs');
-
-const PORT=9000;
-
+const express = require("express");
+const fs = require("fs");
+const cors = require("cors");
+const { appData } = require("./mock");
+const app = express();
+const PORT = 9000;
 
 const __AUDIO_TYPE__ = {
     ROCK: "rock",
@@ -11,6 +11,7 @@ const __AUDIO_TYPE__ = {
     CINEMATIC: "cinematic",
     ACCOUSTIC: "accoustic"
 }
+
 function getDirectories(type){
     return fs.readdirSync(`./music/${type}`);
 }
@@ -31,26 +32,29 @@ function getSongDetails(type, dir){
     }
 }
 
-app.get(`/song`,(req,res)=>{
+app.use(cors());
+app.use('/music', express.static('music'));
+
+app.get(`/song`, (req, res) => {
     const songData = {};
     for(let type in __AUDIO_TYPE__){
         const directoryItems = getDirectories(__AUDIO_TYPE__[type]);
         directoryItems.forEach((item) => {
             const audioData = getSongDetails(__AUDIO_TYPE__[type], item);
-            console.log({audioData});
             if(audioData){
                 if(__AUDIO_TYPE__[type] in songData){
                     songData[__AUDIO_TYPE__[type]].push(audioData);
                 }else{
                     songData[__AUDIO_TYPE__[type]] = [audioData];
                 }
-            }    
-    })
-}
-    res.send("Hello from server");
+            }            
+        })
+    }
+    appData['freelicense'] = songData
+
+    res.status(200).json({ appData });
 });
 
-app.listen(PORT,()=>
-{
-    console.log(`Server listening on port ${PORT}`);  
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
 })
