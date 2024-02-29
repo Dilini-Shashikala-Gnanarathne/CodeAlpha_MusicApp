51a21da667d42f26996afd8eb95103783d2f309f
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-02-29 23:49:36 +0530

    index.server.js file created

--- server/index.server.js
+++ server/index.server.js
@@ -15,11 +15,38 @@ function getDirectories(type){
     return fs.readdirSync(`./music/${type}`);
 }
 
-app.get(`/song`,(req,res)=>{
-    for(let type in __AUDIO_TYPE__){
-        console.log(getDirectories(__AUDIO_TYPE__[type]))
-        
+function getSongDetails(type, dir){
+    try{
+        const path = `music/${type}/${dir}/media`;
+        const audioData = fs.readdirSync(path);
+        const audioInfo = fs.readFileSync(`./music/${type}/${dir}/info.json`);
+        const parsedData = JSON.parse(audioInfo);
+        return {
+            audioFile: `${path}/${audioData[0]}`,
+            avatar: `${path}/${audioData[1]}`,
+            ...parsedData
+        }
+    }catch(error){
+        return false;
     }
+}
+
+app.get(`/song`,(req,res)=>{
+    const songData = {};
+    for(let type in __AUDIO_TYPE__){
+        const directoryItems = getDirectories(__AUDIO_TYPE__[type]);
+        directoryItems.forEach((item) => {
+            const audioData = getSongDetails(__AUDIO_TYPE__[type], item);
+            console.log({audioData});
+            if(audioData){
+                if(__AUDIO_TYPE__[type] in songData){
+                    songData[__AUDIO_TYPE__[type]].push(audioData);
+                }else{
+                    songData[__AUDIO_TYPE__[type]] = [audioData];
+                }
+            }    
+    })
+}
     res.send("Hello from server");
 });
 
